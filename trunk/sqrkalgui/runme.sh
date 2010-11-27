b5e07f06a8daff6a0de5ad5e937c34d3255b3717
saswat852003 <saswat852003@18c8d926-ead1-11de-b362-3513cf04e977>
2010-11-27 05:08:37 +0000

    change to GUI java and shell script

--- trunk/sqrkalgui/runme.sh
+++ trunk/sqrkalgui/runme.sh
@@ -1,6 +1,17 @@
 #/bin/bash
+### First option is 1 if we are starting and 2 if stopping. Second option 
+### is argument to the program
+if [ $1 -eq 1 ] ; then
+	echo 'Running sqrkal daemon process with option' $2
+	xterm -e "sudo sqrkald $1";
+	echo 'Done'
+else                   
+    if [ $1 -eq 2 ] ; then
+		echo 'Running sqrkal daemon process with option' $2
+		skill -9 sqrkald
+		iptables -F
+		echo 'Done'
+	fi
+fi
 
-echo 'Running sqrkal daemon process with option' $1
-xterm -e "sudo sqrkald $1";
-echo 'Done'
 
