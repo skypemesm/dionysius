0eb9401d977d271ecfd53b8d67f4df41bf5d750d
saswat852003 <saswat852003@18c8d926-ead1-11de-b362-3513cf04e977>
2010-11-29 08:38:32 +0000

    no clue if this works.

---
+++ test/testscripts/assess_RE.pl
@@ -0,0 +1,104 @@
+#! /usr/bin/perl
+
+#***************************************** AUTHOR : SASWAT MOHANTY <smohanty [at] cs.tamu.edu> ***********************************************
+
+
+use List::Util qw(shuffle sum max);
+# ---- function to compute probability distribution
+
+sub prob_dist
+{
+
+
+	$nbins = 50; #### NUMBER OF BINS...
+
+	(@thisout) = @_;
+	$maxval = max @thisout;
+	$maxindex = scalar @thisout;
+ 
+	#---- calculate frequencies----
+	$inter = int($maxval / $nbins + 0.5);
+	@freq = (0) x $nbins;
+
+	for ($i = 0; $i <= $maxindex; $i++)
+	{
+		if ($thisout[$i]){
+		 $val = int ($thisout[$i]/$inter);
+		 $freq[$val]++;
+		}
+	}
+
+	$sum = sum @freq;
+	
+	# calculate probabilities
+	for ($i = 0; $i < scalar @freq; $i++)
+	{
+		$freq[$i] /= $sum;	
+	}
+
+	return @freq;
+}
+
+
+
+
+open SIZES,"<","../sizes.txt" or die $!;
+@output = <SIZES>;
+chomp @output;
+close SIZES;
+
+$i=0;
+@inp=();@out=();
+
+foreach $thisout(@output)
+{
+	#print $thisout.":\n";
+	($inp[$i],$out[$i] )= split(/\t/,$thisout);
+	#print $inp[$i]."\n";
+	$i++;
+}
+
+#print "@inp"."\n\n";
+#print "@out"."\n\n";
+
+@in_freq = prob_dist(@inp);
+@out_freq = prob_dist(@out);
+
+print "\n\nDISTRIBUTIONS:\n----------------------------------\n\n";
+print "INPUT\n"."@in_freq"."\n\n";
+print "OUTPUT\n"."@out_freq"."\n\n";
+
+print "******************************************************\n\n";
+#calculate RE
+$re = 0; $sre = 0; $chi = 0; $bd=0; $ir = 0;
+
+for($i = 0; $i< scalar @out_freq; $i++)
+{
+	if ($in_freq[$i] != 0 && $out_freq[$i] != 0)  # for craigs RNG
+	{
+		#print $in_freq[$i].":".$out_freq[$i]."::".($in_freq[$i] * log ($in_freq[$i]/$out_freq[$i]) / log(2) )."\n";
+		$re += ($in_freq[$i] * log ($in_freq[$i]/$out_freq[$i]) / log(2) );
+		$sre += (($in_freq[$i] * log ($in_freq[$i]/$out_freq[$i]) / log(2) ) 
+					+ ($out_freq[$i] * log ($out_freq[$i]/$in_freq[$i]) / log(2)) ); 
+			
+		$chi += (($out_freq[$i] - $in_freq[$i])^2)/$in_freq[$i] ;
+		$bd +=  sqrt($out_freq[$i] * $in_freq[$i]) ;
+		
+		$ir += ( ($out_freq[$i] * log ($out_freq[$i]/(0.5*($in_freq[$i] + $out_freq[$i]) ))) 
+				+ ($in_freq[$i] * log ($in_freq[$i]/(0.5*($in_freq[$i] + $out_freq[$i]))) )  ) / log(2); 
+
+
+	}
+}
+print "RESULTS:\n----------------------------------\n\n";
+print "RELATIVE ENTROPY:\t\t".$re."\n\n";
+print "Symmetric RELATIVE ENTROPY:\t\t".$sre."\n\n";
+print "Chi-Square Distance:\t\t".$chi."\n\n";
+print "Bhattacharya Distance:\t\t".$bd."\n\n";
+print "Information Radius:\t\t".$ir."\n\n";
+ 
+
+
+
+exit 0;
+
